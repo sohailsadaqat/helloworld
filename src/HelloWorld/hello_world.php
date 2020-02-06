fd78e6b5366a8190a06fc76c997b1b08c3d6afd0
Sohail Sadaqat <sohail.sadaqat@risepeople.com>
2020-02-06 14:07:49 -0800

    My First commit for package

---
+++ src/HelloWorld/hello_world.php
@@ -0,0 +1,20 @@
+<?php
+namespace HelloWorld;
+class SayHello
+{
+    public static function world()
+    {
+        return 'Hello World, Composer!';
+    }
+
+    // public static function send_notification(
+    //     $type,
+    //     $time,
+    //     $recipients,
+    //     $data_values,
+    //     $urgent = false,
+    //     $locale = 'en')
+    // {
+
+    // }
+}
